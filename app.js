66461e3a0b8fbb31ccb5d9052392ec52bb7ed82c
ORANGETAN3422 <113481800+ORANGETAN3422@users.noreply.github.com>
2022-09-19 09:32:13 +1000

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 document.getElementById('proceed').onclick = function(){
     document.getElementById('main').classList.replace('active', 'inactive');
     document.getElementById('carpet').classList.replace('inactive', 'active');
-    invis.style.visibility = 'visible';
 }
-let invis = document.querySelectorAll('.visible');
 document.getElementById('p4').onclick = function(){
     document.getElementById('main').classList.replace('inactive', 'active');
     document.getElementById('carpet').classList.replace('active', 'inactive');
-    invis.style.visibility = 'hidden';
 }
