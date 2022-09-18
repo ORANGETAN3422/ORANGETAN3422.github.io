f7c250093a72253138920ad469eecd7d9a0a9e90
ORANGETAN3422 <113481800+ORANGETAN3422@users.noreply.github.com>
2022-09-18 15:32:54 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ document.getElementById('proceed').onclick = function(){
     document.getElementById('main').classList.replace('active', 'inactive');
     document.getElementById('carpet').classList.replace('inactive', 'active');
 }
+document.getElementById('p4').onclick
+{
+    document.getElementById('main').classList.replace('inactive', 'active');
+    document.getElementById('carpet').classList.replace('active', 'inactive');
+}
