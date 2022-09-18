619133d0d76832aad9db1c7023c1e2a3f2b06408
ORANGETAN3422 <113481800+ORANGETAN3422@users.noreply.github.com>
2022-09-19 09:29:49 +1000

    Create app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ document.getElementById('proceed').onclick = function(){
     document.getElementById('carpet').classList.replace('inactive', 'active');
     invis.style.visibility = 'visible';
 }
-let invis = document.querySelectorAll('visible');
+let invis = document.querySelectorAll('.visible');
 document.getElementById('p4').onclick = function(){
     document.getElementById('main').classList.replace('inactive', 'active');
     document.getElementById('carpet').classList.replace('active', 'inactive');
