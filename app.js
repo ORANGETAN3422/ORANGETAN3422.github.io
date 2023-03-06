435c94486d4c61f2660ad5f16477b62b5a89dc25
ORANGETAN3422 <113481800+ORANGETAN3422@users.noreply.github.com>
2023-03-06 16:31:50 +1100

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('proceed').onclick = function(){
-    document.getElementById('main').classList.replace('active', 'inactive');
-    document.getElementById('carpet').classList.replace('inactive', 'active');
-}
-document.getElementById('p4').onclick = function(){
-    document.getElementById('main').classList.replace('inactive', 'active');
-    document.getElementById('carpet').classList.replace('active', 'inactive');
-}
